b8f81fdb37664baeaca6a6bdb7ebcfa7fb3ef46b
neb15161718 <49074896+neb15161718@users.noreply.github.com>
2022-11-09 15:29:32 +0000

    1
    1

--- Assets/Collectibles.cs
+++ Assets/Collectibles.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collectibles : MonoBehaviour
 {
     public static Collectibles Instance;
+    int stars;
+    Text starDisplay;
 
     void Start()
     {
         Instance = this;
+        stars = 0;
+        starDisplay.text = "H";
     }
 
     void Update()
     {
-        
+        Debug.Log(stars);
+        starDisplay.text = stars.ToString();
     }
 
     public void AddStar()
     {
-        Debug.Log("H");
+        stars = stars + 1;
     }
 }
