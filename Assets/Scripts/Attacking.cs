93bdbfe0a652f8d5f79a6b217b23a09af3f19bf5
neb15161718 <49074896+neb15161718@users.noreply.github.com>
2022-11-14 14:23:08 +0000

    1
    1

--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class Attacking : MonoBehaviour
 {
     Animator animator;
+    public int health;
+    public TextMeshProUGUI healthDisplay;
+    Actions playerInputActions;
+    Enemy enemy;
+    public static Attacking Instance;
+    GameObject closest;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        health = 10;
+        Instance = this;
+        closest = null;
     }
 
     void Update()
     {
-        
+        healthDisplay.text = ("Health: " + health.ToString());
     }
 
     public void Attack(InputAction.CallbackContext context)
@@ -22,6 +32,35 @@ public class Attacking : MonoBehaviour
         if (context.performed)
         {
             animator.SetTrigger("Attack");
+            foreach (GameObject enemies in GameObject.FindGameObjectsWithTag("Enemy"))
+            {
+                enemy = enemies.GetComponent<Enemy>();
+                if (enemy.touchingPlayer == true)
+                {
+                    enemy.health = enemy.health - 1;
+                }
+            }
+            closest = FindClosestEnemy();
+            if (closest != null)
+            {
+                transform.LookAt(closest.transform.position);
+            }
         }
+    }
+
+    GameObject FindClosestEnemy()
+    {
+        foreach (GameObject enemies in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            float distance = Mathf.Infinity;
+            Vector3 difference = new Vector3(enemies.transform.position.x, 0, enemies.transform.position.z) - new Vector3(transform.position.x, 0, transform.position.z);
+            float diff = difference.sqrMagnitude;
+            if (diff < distance & diff < 10)
+            {
+                closest = enemies;
+                distance = diff;
+            }
+        }
+        return closest;
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    Vector3 target;
+    Animator animator;
+    public int health;
+    public bool touchingPlayer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        health = 3;
+        touchingPlayer = false;
+    }
+
+    void Update()
+    {
+        target = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+        transform.LookAt(target);
+        transform.position = Vector3.MoveTowards(transform.position, target, 0.05f);
+        if (transform.position == target)
+        {
+            animator.SetBool("Moving", false);
+        }
+        else
+        {
+            animator.SetBool("Moving", true);
+        }
+        Debug.Log(health);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            touchingPlayer = true;
+            StartCoroutine(Attack());
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            touchingPlayer = false;
+        }
+    }
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1);
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(1);
+        if (touchingPlayer == true)
+        {
+            Attacking.Instance.health = Attacking.Instance.health - 1;
+        }
+    }
+}
